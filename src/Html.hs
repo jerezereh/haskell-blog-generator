63c3b5d8cef52c1086b3faf62f702f3d658ddfa3
jerezereh <mavop01@gmail.com>
2023-09-19 21:52:42 -0700

    Chapter 3.6 completed

--- src/Html.hs
+++ src/Html.hs
@@ -19,24 +19,24 @@ type Title
     = String
 
 html_ :: Title -> Structure -> Html
-html_ title content = 
+html_ title content =
     Html
         ( el "html"
-            ( el "head" (el "title" title)
+            ( el "head" (el "title" (escape title))
                 <>  el "body" (getStructureString content)))
 
 p_ :: String -> Structure
-p_ = Structure . el "p"
+p_ = Structure . el "p" . escape
 
 h1_ :: String -> Structure
-h1_ = Structure . el "h1"
+h1_ = Structure . el "h1" . escape
 
 el :: String -> String -> String
-el tag content = 
+el tag content =
     "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
 append_ :: Structure -> Structure -> Structure
-append_ c1 c2 = 
+append_ c1 c2 =
     Structure (getStructureString c1 <> getStructureString c2)
 
 getStructureString :: Structure -> String
@@ -45,6 +45,20 @@ getStructureString content =
     Structure str -> str
 
 render :: Html -> String
-render html = 
-    case html of 
+render html =
+    case html of
         Html str -> str
+
+escape :: String -> String
+escape =
+    let
+        escapeChar c =
+            case c of
+            '<' -> "&lt;"
+            '>' -> "&gt;"
+            '&' -> "&amp;"
+            '"' -> "&quot;"
+            '\'' -> "&#39;"
+            _ -> [c]
+    in
+        concatMap escapeChar
